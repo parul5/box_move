692c5c54b9572e63949913a80bad31823b8bac73
Parul Kumar <prlkumar96@gmail.com>
2022-02-17 12:45:58 +0530

    fix boxes keys

--- src/components/canvas/canvas.js
+++ src/components/canvas/canvas.js
@@ -5,8 +5,8 @@ import './canvas.css'
 export default function Canvas({ boxes, deleteAction, isDisabled }) {
     return (
         <div className="canvas">
-            {boxes.map((box, index) => {
-                return <Box id={box.id} key={index} deleteAction={deleteAction} isDisabled={isDisabled} />
+            {boxes.map((box) => {
+                return <Box id={box.id} key={box.id} deleteAction={deleteAction} isDisabled={isDisabled} />
             })}
         </div>
     )
